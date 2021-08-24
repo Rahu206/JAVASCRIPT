ae1f5abd7e8143e68b73619c93c30ff2dd6e5851
Rahu206 <rahuldeshpande93@gmail.com>
2021-08-25 00:01:58 +0530

    ADDED REPEATATION PROBLEM BY USING FOR LOOP

---
+++ Selection_by_Case/ClassAndObject.js
@@ -0,0 +1,11 @@
+class Employee{
+    constructor(id,name,salary){
+        this.this
+        this.name=name;
+        this.salary=salary;
+
+    }
+}
+const employee1=new Employee(1,"Uma",1000)
+
+console.log(employee1.name)
